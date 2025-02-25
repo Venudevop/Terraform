ee7c2df2a8e398833f5cd43704b8ca35b6a4bf99
venu <venuking375@gmail.com>
2025-02-25 06:54:08 +0530

    terraform

---
+++ main.tf
@@ -0,0 +1,25 @@
+
+	provider "aws"{
+		secret_key = "AlaJG5VLwR/OXFcQ2gAVV8s296uRhHzNc+mFT0dF"
+		access_key ="AKIATG6MGGVDPKR7FAAL"
+        region = "ap-south-1"
+
+
+    }
+	
+
+    resource "aws_s3_bucket" "s3" {
+
+        bucket = "venuking12345"
+
+      
+    }
+
+    terraform {
+        backend "s3" {
+            bucket ="venuking12345"
+            key="devop/venu/terraform.tfstate"
+            region = "ap-south-1"
+          
+        }
+    }
---
+++ provider.tf
@@ -0,0 +1,8 @@
+
+	provider "aws"{
+		secret_key = "AlaJG5VLwR/OXFcQ2gAVV8s296uRhHzNc+mFT0dF"
+		access_key ="AKIATG6MGGVDPKR7FAAL"
+        region = "ap-south-1"
+
+
+    }
