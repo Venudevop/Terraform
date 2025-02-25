c170385d073c72740ce58ce23d791c209f59785b
Venudevop <168158656+Venudevop@users.noreply.github.com>
2025-02-25 10:40:48 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,22 +1,14 @@
-provider "aws"{
-	region = "ap-south-1"
+provider "aws" {
+  region = "ap-south-1"
+}
 
+resource "aws_s3_bucket" "s3" {
+  bucket = "venuking12345"
+}
 
-    }
-	
-
-    resource "aws_s3_bucket" "s3" {
-
-        bucket = "venuking12345"
-
-      
-    }
-
-    terraform {
-        backend "s3" {
-            bucket ="venuking12345"
-            key="devop/venu/terraform.tfstate"
-            region = "ap-south-1"
-          
-        }
-    }
+terraform {
+  backend "s3" {
+    bucket = "venuking12345"
+    key    = "devop/venu/terraform.tfstate"
+    region = "ap-south-1"
+ 
