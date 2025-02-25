59c453c9607a927faf554459fbe6f3d30753353b
Venudevop <168158656+Venudevop@users.noreply.github.com>
2025-02-25 08:01:01 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,11 +15,11 @@
       
     }
 
-    terraform {
-        backend "s3" {
-            bucket ="venuking12345"
-            key="devop/venu/terraform.tfstate"
-            region = "ap-south-1"
+#    terraform {
+  #      backend "s3" {
+  #          bucket ="venuking12345"
+  #          key="devop/venu/terraform.tfstate"
+  #          region = "ap-south-1"
           
-        }
-    }
+ #       }
+  #  }
